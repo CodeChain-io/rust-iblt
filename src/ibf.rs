d35e5efa63f4015d13c21228977cd42bfa78697a
Kais-DkM <kais0903@gmail.com>
2018-05-05 22:37:15 +0900

    Implement IBF creation

--- src/ibf.rs
+++ src/ibf.rs
@@ -1,23 +1,42 @@
 use std::collections::HashSet;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{BuildHasher, BuildHasherDefault, Hash};
 use std::marker::PhantomData;
 
 struct Item {
-    count_sum: usize,
+    count: usize,
     val_sum: Box<[u8]>,
     hash_sum: u64,
 }
 
-pub struct IBF<T> {
+pub struct IBF<T, S = BuildHasherDefault<DefaultHasher>> {
     size: usize,
     map: Vec<Item>,
+    hash_builder: S,
     phantom: PhantomData<*const T>,
 }
 
-impl<T> IBF<T> {
+impl<T: Hash + Into<Box<[u8]>>> IBF<T, BuildHasherDefault<DefaultHasher>> {
     pub fn new(size: usize) -> Self {
+        Self::with_hasher(size, Default::default())
+    }
+}
+
+impl<T: Hash + Into<Box<[u8]>>, S: BuildHasher> IBF<T, S> {
+    pub fn with_hasher(size: usize, hash_builder: S) -> Self {
+        let mut map = Vec::with_capacity(size);
+        for _ in 0..size {
+            map.push(Item {
+                count: 0,
+                val_sum: Box::new([]),
+                hash_sum: 0,
+            });
+        }
+
         Self {
             size,
-            map: Vec::with_capacity(size),
+            map,
+            hash_builder,
             phantom: PhantomData,
         }
     }
